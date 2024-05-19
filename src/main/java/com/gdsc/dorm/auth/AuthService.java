655f0ed2a7c80093ea9e6d47e9b6194c5ada44f0
Koo-EunSung <ku903@naver.com>
2024-05-19 23:53:33 +0900

    Feat: 비밀번호 저장 시 암호화

--- src/main/java/com/gdsc/dorm/auth/AuthService.java
+++ src/main/java/com/gdsc/dorm/auth/AuthService.java
@@ -7,12 +7,14 @@ import com.gdsc.dorm.member.data.dto.res.MemberGetRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder pwEncoder;'
 
     public ResponseEntity<MemberGetRes> signUp(SignUpReq req) {
         if(req == null) {
@@ -22,10 +24,11 @@ public class AuthService {
             throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
         }
         // 추후 예외처리할 것
+        String bcryptPw = pwEncoder.encode(req.getPassword());
         Member newMember = Member.builder()
                 .name(req.getName())
                 .email(req.getEmail())
-                .password(req.getPassword()) //암호화 없이 db에 저장 중. 추후 수정 필요
+                .password(bcryptPw)
                 .gender(req.getGender())
                 .dorm(req.getDorm())
                 .build();
--- src/main/java/com/gdsc/dorm/config/WebSecurityConfig.java
+++ src/main/java/com/gdsc/dorm/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
@@ -28,5 +29,10 @@ public class WebSecurityConfig {
                     csrf.disable();
                 })
                 .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
