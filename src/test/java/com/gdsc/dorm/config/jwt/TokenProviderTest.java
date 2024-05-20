e2c42bc9a7877e2b79dc76cf5498e502ce6a7190
Koo-EunSung <ku903@naver.com>
2024-05-21 02:50:20 +0900

    Test: JWT TokenProvider 테스트

--- src/test/java/com/gdsc/dorm/config/jwt/TokenProviderTest.java
+++ src/test/java/com/gdsc/dorm/config/jwt/TokenProviderTest.java
@@ -1,15 +1,21 @@
 package com.gdsc.dorm.config.jwt;
 
+import static org.assertj.core.api.Assertions.*;
+
 import com.gdsc.dorm.member.MemberRepository;
 import com.gdsc.dorm.member.data.Member;
 import com.gdsc.dorm.member.type.Dorm;
 import com.gdsc.dorm.member.type.Gender;
 import io.jsonwebtoken.Jwts;
 import java.time.Duration;
+import java.util.Date;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @SpringBootTest
 public class TokenProviderTest {
@@ -44,6 +50,65 @@ public class TokenProviderTest {
                 .getBody()
                 .get("id", Long.class);
 
-        Assertions.assertThat(memberId).isEqualTo(member.getId());
+        assertThat(memberId).isEqualTo(member.getId());
+    }
+
+    @Test
+    void validToken_invalid() {
+        //given
+        String token = JwtFactory.builder()
+                .expiration(new Date(new Date().getTime() - Duration.ofDays(7).toMillis())) // 만료 기한을 7일 전으로 설정
+                .build()
+                .createToken(jwtProperties);
+
+        //when
+        boolean result = tokenProvider.validToken(token);
+
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void validToken_valid() {
+        //given
+        String token = JwtFactory.withDefaultValues().createToken(jwtProperties);
+
+        //when
+        boolean result = tokenProvider.validToken(token);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void getAuthentication() {
+        //given
+        String memberEmail = "testMember@email.com";
+        String token = JwtFactory.builder()
+                .subject(memberEmail)
+                .build()
+                .createToken(jwtProperties);
+
+        //when
+        Authentication authentication = tokenProvider.getAuthentication(token);
+
+        //then
+        assertThat(((UserDetails) authentication.getPrincipal()).getUsername()).isEqualTo(memberEmail);
+    }
+
+    @Test
+    void getMemberrId() {
+        //given
+        Long memberId = 1L;
+        String token = JwtFactory.builder()
+                .claims(Map.of("id", memberId))
+                .build()
+                .createToken(jwtProperties);
+
+        //when
+        Long memberIdByToken = tokenProvider.getUserId(token);
+
+        //then
+        assertThat(memberIdByToken).isEqualTo(memberId);
     }
 }
