94cddb97e0fdef5bb649d9c3fbb0cd7525b2c083
Koo-EunSung <ku903@naver.com>
2024-05-23 20:27:51 +0900

    Feat: 로그아웃 기능 & 시큐리티 설정

--- src/main/java/com/gdsc/dorm/auth/AuthController.java
+++ src/main/java/com/gdsc/dorm/auth/AuthController.java
@@ -4,8 +4,13 @@ import com.gdsc.dorm.auth.data.dto.req.LoginReq;
 import com.gdsc.dorm.auth.data.dto.req.SignUpReq;
 import com.gdsc.dorm.auth.data.dto.res.LoginRes;
 import com.gdsc.dorm.member.data.dto.res.MemberGetRes;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +30,13 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<LoginRes> login(@RequestBody LoginReq req) {
         return authService.login(req);
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext()
+                .getAuthentication());
+
+        return "redirect:/main"; //임시로 지정한 리다이렉션
     }
 }
--- src/main/java/com/gdsc/dorm/config/WebSecurityConfig.java
+++ src/main/java/com/gdsc/dorm/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,10 +24,14 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests((authorizeHttpRequests) -> {
-                    authorizeHttpRequests.requestMatchers("/api/auth/signup").permitAll();
+                    authorizeHttpRequests.requestMatchers("/api/auth/*").permitAll();
                 })
                 .csrf((csrf) -> {
                     csrf.disable();
+                })
+                .formLogin(Customizer.withDefaults())
+                .logout((logout) -> {
+                    logout.invalidateHttpSession(true);
                 })
                 .build();
     }
