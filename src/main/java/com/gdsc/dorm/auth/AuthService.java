eaba99219bdb31b65e37bb51304d67caf0ae0ec9
Koo-EunSung <ku903@naver.com>
2024-06-10 18:14:35 +0900

    Feat: 회원가입 시 예외처리 추가

--- src/main/java/com/gdsc/dorm/auth/AuthService.java
+++ src/main/java/com/gdsc/dorm/auth/AuthService.java
@@ -28,13 +28,10 @@ public class AuthService {
     private final TokenProvider tokenProvider;
 
     public ResponseEntity<MemberGetRes> signUp(SignUpReq req) {
-        if(req == null) {
-            throw new IllegalArgumentException("잘못된 요청입니다.");
-        }
         if(memberRepository.existsByEmail(req.getEmail())) {
-            throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
+            throw new IllegalArgumentException("signup: 이미 존재하는 사용자입니다.");
         }
-        // 추후 예외처리할 것
+
         String bcryptPw = pwEncoder.encode(req.getPassword());
         Member newMember = Member.builder()
                 .name(req.getName())
